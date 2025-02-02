ed83efee8f1e4be9fd783ca106d8df7ba4fc2e61
Gaston Ariel Perrone <gastonarielp1812@gmail.com>
2025-02-02 16:48:02 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+// Cargar el archivo JSON
+const jsonData = JSON.parse(fs.readFileSync('APIFIFA.json', 'utf8'));
+
+// Ruta para obtener los datos
+app.get('/api/data', (req, res) => {
+  res.json(jsonData);
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
